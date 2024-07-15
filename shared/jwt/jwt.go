720ea73091f9e1aea1e410617b2a4ddcebd20b00
azka1415 <rafifzaydan4@gmail.com>
2024-07-16 00:54:36 +0700

    fix: update jwt

--- shared/jwt/jwt.go
+++ shared/jwt/jwt.go
@@ -3,27 +3,53 @@ package jwt
 import (
 	"time"
 
+	"github.com/azka-zaydan/synapsis-test/configs"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-type TokenData struct {
-	Username string
+type JwtService struct {
+	cfg *configs.Config
+	key []byte
 }
 
-func NewToken(username string) *jwt.Token {
-	claims := jwt.MapClaims{
-		"name": username,
-		"exp":  time.Now().Add(time.Hour * 72).Unix(),
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func NewJwtService(cfg *configs.Config) *JwtService {
+	key := []byte(cfg.JWT.Key)
+	return &JwtService{
+		cfg: cfg,
+		key: key,
 	}
-	// Create token
+}
+
+func (s *JwtService) GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token
+	return token.SignedString(s.key)
 }
 
-func ParseToken(token *jwt.Token) *TokenData {
-	claims := token.Claims.(jwt.MapClaims)
-	name := claims["name"].(string)
-	return &TokenData{
-		Username: name,
+func (s *JwtService) ValidateJWT(tokenStr string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return s.key, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return nil, err
+		}
+		return nil, err
 	}
+	if !token.Valid {
+		return nil, err
+	}
+	return claims, nil
 }
